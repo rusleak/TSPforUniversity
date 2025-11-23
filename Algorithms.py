3acc38caeaf30d2be7fa44655f2a3dd22582b849
rusleak <hotlinetony2@gmail.com>
2025-11-23 23:58:48 +0100

    Task 10 + Optimisation

--- Algorithms.py
+++ Algorithms.py
@@ -1,44 +1,104 @@
-# class Algorithms:
-#
-#     @staticmethod
-#     def greedy_algorithm(routes, starting_city):
-#         internal_routes = routes.copy()
-#         internal_routes.remove(starting_city)
-#
-#         path = [starting_city]
-#         current_city = starting_city
-#
-#         while internal_routes:
-#             min_city = None
-#             min_dist = float('inf')
-#
-#             for route in internal_routes:
-#                 dist = current_city.distance_to(route)
-#                 if dist < min_dist:
-#                     min_dist = dist
-#                     min_city = route
-#
-#             path.append(min_city)
-#             current_city = min_city
-#             internal_routes.remove(current_city)
-#
-#         fitness = calculate_fitness(path)
-#         result = {tuple(path): fitness}
-#         return result
-#
-#     @staticmethod
-#     def find_best_starting_city_greedy(coordinates):
-#         best_city = None
-#         best_fitness = float('inf')
-#
-#         for city in coordinates:
-#             result = Algorithms.greedy_algorithm(coordinates, city)
-#             path, fitness = list(result.items())[0]
-#
-#             info(result)
-#
-#             if fitness < best_fitness:
-#                 best_fitness = fitness
-#                 best_city = path[0]
-#
-#         return best_city, best_fitness
+import random
+
+
+class Algorithms:
+
+    @staticmethod
+    def calculate_fitness(solution):
+        total_distance = 0.0
+        for i in range(len(solution) - 1):
+            total_distance += solution[i].distance_to(solution[i + 1])
+        total_distance += solution[-1].distance_to(solution[0])
+        return total_distance
+
+    @staticmethod
+    def info(routes):
+        if isinstance(routes, dict):
+            for path, fitness in routes.items():
+                for city in path:
+                    print(f"{city.number} -> ", end="")
+                print(path[0].number)
+                print(f"Fitness: {fitness:.2f}")
+                print("-" * 30)
+        elif isinstance(routes, list):
+            for city in routes:
+                print(f"{city.number} -> ", end="")
+            print(routes[0].number)
+            print("Fitness: N/A")
+            print("-" * 30)
+        else:
+            print("Unsupported type")
+
+    @staticmethod
+    def greedy_algorithm(routes, starting_city):
+        internal_routes = routes.copy()
+        internal_routes.remove(starting_city)
+
+        path = [starting_city]
+        current_city = starting_city
+
+        while internal_routes:
+            min_city = None
+            min_dist = float('inf')
+
+            for route in internal_routes:
+                dist = current_city.distance_to(route)
+                if dist < min_dist:
+                    min_dist = dist
+                    min_city = route
+
+            path.append(min_city)
+            current_city = min_city
+            internal_routes.remove(current_city)
+
+        fitness = Algorithms.calculate_fitness(path)
+        result = {tuple(path): fitness}
+        return result
+
+    @staticmethod
+    def best_greedy_starting_city(coordinates):
+        best_city = None
+        best_fitness = float('inf')
+
+        for city in coordinates:
+            result = Algorithms.greedy_algorithm(coordinates, city)
+            path, fitness = list(result.items())[0]
+
+            Algorithms.info(result)
+
+            if fitness < best_fitness:
+                best_fitness = fitness
+                best_city = path[0]
+
+        return best_city, best_fitness
+
+    @staticmethod
+    def random_routes_analysis(coordinates, n=100):
+        best_route_map = {}
+        routes_map = {}
+
+        for i in range(n + 1):
+            current_route = Algorithms.generate_random_route(coordinates)
+            current_fitness = Algorithms.calculate_fitness(current_route)
+
+            route_key = tuple(
+                current_route)
+            routes_map[route_key] = current_fitness
+
+            # updating
+            if len(best_route_map) == 0 or current_fitness < list(best_route_map.values())[0]:
+                best_route_map = {route_key: current_fitness}
+
+        print("ROUTES MAP")
+        Algorithms.info(routes_map)
+
+        print("Best route by 100 randoms choice:")
+        Algorithms.info(best_route_map)
+
+        return best_route_map, routes_map
+
+    @staticmethod
+    def generate_random_route(cities):
+        solution = cities.copy()
+        random.shuffle(solution)
+        return solution
--- Main.py
+++ Main.py
@@ -1,110 +1,35 @@
 import random
+from turtledemo.penrose import inflatedart
 
-from Coordinates import Coordinates
+from Algorithms import Algorithms
 from Parser import Parser
+
 "-------------------------TASK4-----------------------------------"
 coordinates_berlin_11 = Parser.read_tsp_file("/Users/rusleak/Downloads/berlin11_modified.tsp")
 coordinates_berlin_52 = Parser.read_tsp_file("/Users/rusleak/Downloads/berlin52.tsp")
 
-def generate_random_route(cities):
-    solution = cities.copy()
-    random.shuffle(solution)
-    return solution
+random_route = Algorithms.generate_random_route(coordinates_berlin_11)
 
-random_route = generate_random_route(coordinates_berlin_11)
 "------------------------TASK5------------------------------------"
-
-def calculate_fitness(solution):
-    total_distance = 0.0
-    for i in range(len(solution) - 1):
-        total_distance += solution[i].distance_to(solution[i + 1])
-    # + from last city to origin
-    total_distance += solution[-1].distance_to(solution[0])
-    return total_distance
-
-fitness = calculate_fitness(random_route)
+fitness = Algorithms.calculate_fitness(random_route)
 
 
-"------------------------TASK6------------------------------------"
-
-def info(routes_dict):
-    for path, fitness in routes_dict.items():
-        for city in path:
-            print(f"{city.number} -> ", end="")
-        print(path[0].number)
-        print(f"Fitness: {fitness:.2f}")
-        print("-" * 30)
+print("------------------------TASK7------------------------------------")
+greedy_result = Algorithms.greedy_algorithm(random_route, random_route[0])
+Algorithms.info(greedy_result)
 
 
-"------------------------TASK7------------------------------------"
-#Starting city is object of Coordinates, not an index
-def greedy_algorithm(routes, starting_city):
-    internal_routes = routes.copy()
-    internal_routes.remove(starting_city)
-
-    path = [starting_city]
-    current_city = starting_city
-
-    while internal_routes:
-        min_city = None
-        min_dist = float('inf')
-
-        for route in internal_routes:
-            dist = current_city.distance_to(route)
-            if dist < min_dist:
-                min_dist = dist
-                min_city = route
-
-        path.append(min_city)
-        current_city = min_city
-        internal_routes.remove(current_city)
-
-    fitness = calculate_fitness(path)
-
-    result = {tuple(path): fitness}
-
-    # info(path)
-
-    return result
-
-greedy_algorithm(coordinates_berlin_11, coordinates_berlin_11[0])
+print("------------------------TASK8------------------------------------")
+best_city, best_fit = Algorithms.best_greedy_starting_city(coordinates_berlin_11)
+print("Best starting city by greedy algorithm BERLIN11:", best_city, "fitness:", best_fit)
 
 
+print("------------------------TASK9------------------------------------")
+best_route_fitness, dict_of_routes_fitness = Algorithms.random_routes_analysis(coordinates_berlin_11, 100)
 
-"------------------------TASK8------------------------------------"
+print("------------------------TASK10------------------------------------")
+best_city, best_fit = Algorithms.best_greedy_starting_city(coordinates_berlin_52)
+print("Best starting city by greedy algorithm  BERLIN52:", best_city, "fitness:", best_fit)
 
-print("TASK 8")
-
-best_starting_city_greedy = None
-best_fitness_greedy = float('inf')
-
-for city in coordinates_berlin_11:
-    result = greedy_algorithm(coordinates_berlin_11, city)
-    # result = {tuple(path): fitness}
-    path, fitness = list(result.items())[0]  # [(tuple, float)] → (tuple, float)
-
-    info(result)
-    if fitness < best_fitness_greedy:
-        best_fitness = fitness
-        best_starting_city = path[0]
-print("Best starting city by greedy algorithm : " + str(best_starting_city_greedy) + " fitness : " + str(best_fitness_greedy))
-"------------------------TASK9------------------------------------"
-print("TASK 9")
-
-
-best_starting_city_hundred = None
-best_starting_fitness_hundred = float('inf')
-routes_map = {}
-
-for i in range(101):
-    current_route = generate_random_route(coordinates_berlin_11)
-    current_fitness = calculate_fitness(current_route)
-    routes_map[tuple(current_route)] = current_fitness
-
-    if current_fitness < best_starting_fitness_hundred:
-        best_starting_fitness_hundred = current_fitness
-        best_starting_city_hundred = current_route[0]
-print("ROUTES MAP")
-info(routes_map)
-print(f"Best starting city by 100 mixes: {best_starting_city_hundred.number}")
-print(f"Fitness: {best_starting_fitness_hundred:.2f}")
+best_route_fitness, dict_of_routes_fitness = Algorithms.random_routes_analysis(coordinates_berlin_52, 100)
+Algorithms.info(best_route_fitness)
